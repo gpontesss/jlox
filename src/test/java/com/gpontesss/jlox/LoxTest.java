82a966889ef6ef1c82871357c29de98a847e5039
gpontesss <pontes.guisilva@gmail.com>
2020-06-09 15:12:24 -0300

    Initial commit

---
+++ src/test/java/com/gpontesss/jlox/LoxTest.java
@@ -0,0 +1,13 @@
+package com.gpontesss.jlox;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class LoxTest {
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue(true);
+    }
+}
